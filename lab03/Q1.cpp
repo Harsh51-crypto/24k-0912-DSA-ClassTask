446534941e90be9d9abe61cd2b54807f4719e8bf
Harsh Dembla <harshdembla80@gmail.com>
2025-09-13 04:24:38 -0700

    completed Question 1

--- lab03/Q1.cpp
+++ lab03/Q1.cpp
@@ -1,87 +1,138 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
+class Node
+{
 
-    public:
-    Node *next;
-    string book;
+public:
+   Node *next;
+   string book;
 
+   Node()
+   {
+      next = NULL;
+      book = "";
+   }
 
-    Node(){
-        next=NULL;
-        book="";
-    }
-
-    Node(string name):book(name){
-        next=NULL;
-    }
-
+   Node(string name) : book(name)
+   {
+      next = NULL;
+   }
 };
 
 class LL
 {
-
-
-    Node *head;
-    Node *tail;
 public:
+   Node *head;
+   Node *tail;
 
-void insertAtTail(string name){
+   LL()
+   {
+      head = NULL;
+      tail = NULL;
+   }
+   void insertAtTail(string name)
+   {
 
-    Node *temp=new Node(name);
+      Node *temp = new Node(name);
 
-    if(tail!=NULL){
-        
-        tail->next=temp;
-         tail=temp;
-        
-    }
-    else {
-        tail=temp;
-    }
-}
+      if (tail != NULL)
+      {
 
-void insertAtHead(string name){
-    Node *temp=new Node(name);
+         tail->next = temp;
+         tail = temp;
+      }
+      else
+      {
+         tail = temp;
+         head = temp;
+      }
+   }
 
-    if(head!=NULL){
-        temp->next=head;
-        head=temp;
-    }
-    else{
+   void insertAtHead(string name)
+   {
+      Node *temp = new Node(name);
 
-        head=temp;
-    }
+      if (head != NULL)
+      {
+         temp->next = head;
+         head = temp;
+      }
+      else
+      {
 
+         head = temp;
+         tail = temp;
+      }
+   }
 
+   bool search(string target)
+   {
 
-}
+      Node *temp = head;
+      while (temp != NULL)
+      {
+         if (temp->book == target)
+            return true;
+         temp = temp->next;
+      }
+      return false;
+   }
 
-void insertAtPos(string target,string name){
+   void insertAtPos(string target, string name)
+   {
 
+      Node *temp = head;
+      Node *insert = new Node(name);
 
-    Node *temp=head;
-        Node *insert=new Node(name);
-        
-    while(temp!=NULL){
+      while (temp != NULL)
+      {
 
-        if(temp->book==target) {
-            
-        }
-    }
+         if (temp->book == target)
+         {
+            insert->next = temp->next;
+            temp->next = insert;
+            return;
+         }
+         temp = temp->next;
+      }
+   }
 
-}
+   void print()
+   {
 
+      Node *temp = head;
 
-
-
+      while (temp != NULL)
+      {
+         cout << temp->book << endl;
+         temp = temp->next;
+      }
+   }
 };
 
+int main()
+{
+   LL l;
+   l.insertAtTail("Data Structure");
+   l.insertAtTail("Operating System");
+   l.insertAtTail("Computer Networks");
+   l.insertAtTail("Database System");
 
-int main(){
+   l.print();
+   cout << endl;
 
+   l.insertAtHead("Artifical Intelligence");
+   l.print();
 
+   cout << endl;
+   l.insertAtTail("Machine Learning");
 
-    return 0;
+   l.print();
+   cout << endl;
+
+   l.insertAtPos("Operating System", "Cyber Security");
+   l.print();
+   cout << endl;
+   return 0;
 }
